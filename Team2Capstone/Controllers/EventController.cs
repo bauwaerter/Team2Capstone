873c87e5d22a086f4c0f1cf7a7507360653be64c
unknown <aemrick@S21244.sogeti.com>
2015-02-03 20:24:24 -0500

    event view

--- Team2Capstone/Controllers/EventController.cs
+++ Team2Capstone/Controllers/EventController.cs
@@ -17,24 +17,36 @@ namespace Team2Capstone.Controllers
 
         public ActionResult Create()
         {
-            return View();
+            return View(new Models.Event());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Models.Event model)
+        {
+            return null;
         }
 
         public ActionResult Edit(int id)
         {
             var _devEventManager = new DevEventManager();
-            //var ev = _devEventManager.GetEventById(x.Id == id);
+            var ev = _devEventManager.GetEventById(id);
             
-            return View();
+            return View(ev);
         }
 
+        [HttpPost]
+        public ActionResult Edit(Models.Event model)
+        {
+            var _devEventManager = new DevEventManager();
+            return null;
+        }
         //get event details
         public ActionResult Details(int id)
         {
             
             var _eventDetail = new DevEventManager();
-            //var ed = _eventDetail.GetEventDetails(x.Id == id);
-            return View();
+            var ev = _eventDetail.GetEventById(id);
+            return View(ev);
         }
     }
 }
